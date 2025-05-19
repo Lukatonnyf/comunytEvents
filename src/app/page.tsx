2a10f0feb775367258170d2157f3d1d1ca6a7145
lukatonny <lukatonnyferreira16@gmail.com>
2025-05-19 19:05:25 -0300

    aaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
       <Header />
 
       <div className=" border  flex flex-wrap ">
-        <AsideCard className="md:min-w-[5dvw] md:flex-1 " />
-        <HomePage className="md:flex-3 md:min-w-[300px] " />
+        <AsideCard className="md:min-w-[16dvw] md:flex-1 " />
+        <HomePage className="md:flex-5 md:min-w-[300px] " />
 
       </div>
     </div >
