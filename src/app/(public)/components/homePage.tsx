ada7e2dc895f2b1a4dba679367a92b46df0a0054
lukatonny <lukatonnyferreira16@gmail.com>
2025-05-31 02:42:32 -0300

    tentando concertar erro do homepage de rolagem...

--- src/app/(public)/components/homePage.tsx
+++ src/app/(public)/components/homePage.tsx
@@ -75,7 +75,7 @@ export default function HomePage({ className }: { className: string }) {
           <Card className='w-full md:w-[100rem] lg:max-w-[19rem] border-l-4 border-bordercustomizada h-[22rem]
           bg-secondary p-0 flex flex-col justify-between pb-4 '>
             <div className='flex flex-col rounded-t-xl justify-center items-center bg-tertiary  h-[15dvh]  '>
-              <h1 className='font-bold  text-3xl'>22</h1>
+              <h1 className='font-bold  text-3xl'>25</h1>
               <p>MAI</p>
             </div>
             {/* lg:h-[15dvh] */}
