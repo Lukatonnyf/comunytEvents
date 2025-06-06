6166c9b93b5c4875b3ec622b2c1a654a3411f010
lukatonny <lukatonnyferreira16@gmail.com>
2025-06-06 00:58:22 -0300

    aa

--- src/app/(public)/components/homePage.tsx
+++ src/app/(public)/components/homePage.tsx
@@ -48,7 +48,7 @@ export default function HomePage({ className }: { className: string; }) {
 
       // Usuário está logado → redirecionar
       router.push(`/profile/${decoded.userId}`)
-    } catch (error) {
+    } catch {
       // Token inválido
       localStorage.removeItem('token')
       router.push('/login')
--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -34,87 +34,92 @@ export default function Login() {
       } else {
         alert("Credenciais inválidas: " + error);
       }
-    } catch (error: any) {
-      console.error('Erro ao enviar dados:', error);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error('Erro ao enviar dados:', error.message);
+      }
 
-      // 🛑 Quando o erro vem com status (como 401), a resposta real está em:
-      console.log("🔴 Erro com response.data:", error.response?.data);
-
-      alert("Erro no login: " + (error.response?.data?.error || "Erro desconhecido"));
+      // Se for erro do Axios
+      if (axios.isAxiosError(error)) {
+        console.log("🔴 Erro com response.data:", error.response?.data);
+        alert("Erro no login: " + (error.response?.data?.error || "Erro desconhecido"));
+      } else {
+        alert("Erro inesperado ao tentar logar.");
+      }
     }
-  };
 
-  const createAccount = () => {
-    router.push("/register");
-  };
 
-  return (
-    <div>
+    const createAccount = () => {
+      router.push("/register");
+    };
+
+    return (
       <div>
-        <button
-          onClick={() => router.back()}
-          className="bg-tertiary rounded-full p-2 mt-5 ml-5 transition-all duration-500 hover:bg-secondary"
-        >
-          <ArrowLeft className="size-5" />
-        </button>
-      </div>
+        <div>
+          <button
+            onClick={() => router.back()}
+            className="bg-tertiary rounded-full p-2 mt-5 ml-5 transition-all duration-500 hover:bg-secondary"
+          >
+            <ArrowLeft className="size-5" />
+          </button>
+        </div>
 
-      <FormWrapper<FormValues>
-        defaultValues={{ email: "", password: "" }}
-        onSubmit={handleSubmit}
-        className="flex flex-col justify-center items-center
+        <FormWrapper<FormValues>
+          defaultValues={{ email: "", password: "" }}
+          onSubmit={handleSubmit}
+          className="flex flex-col justify-center items-center
           fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50
           w-6/7 lg:w-[35rem] p-5 gap-5 bg-secondary rounded-xl"
-      >
-        {({ register }) => (
-          <div className="w-full flex flex-col gap-5">
-            <section>
-              <h1>Login</h1>
-            </section>
+        >
+          {({ register }) => (
+            <div className="w-full flex flex-col gap-5">
+              <section>
+                <h1>Login</h1>
+              </section>
 
-            <label className="w-full flex flex-col gap-2">
-              Email Cadastrado
-              <input
-                {...register("email")}
-                placeholder="Digite o email da sua conta"
-                className="bg-tertiary border-1 p-2 rounded-sm outline-none"
-              />
-            </label>
+              <label className="w-full flex flex-col gap-2">
+                Email Cadastrado
+                <input
+                  {...register("email")}
+                  placeholder="Digite o email da sua conta"
+                  className="bg-tertiary border-1 p-2 rounded-sm outline-none"
+                />
+              </label>
 
-            <label className="w-full flex flex-col gap-2">
-              Senha
-              <input
-                type="password"
-                {...register("password")}
-                placeholder="Digite a senha da sua conta"
-                className="bg-tertiary border-1 p-2 rounded-sm outline-none"
-              />
-              <p className="text-blue-500 cursor-pointer">esqueci minha senha...</p>
-            </label>
+              <label className="w-full flex flex-col gap-2">
+                Senha
+                <input
+                  type="password"
+                  {...register("password")}
+                  placeholder="Digite a senha da sua conta"
+                  className="bg-tertiary border-1 p-2 rounded-sm outline-none"
+                />
+                <p className="text-blue-500 cursor-pointer">esqueci minha senha...</p>
+              </label>
 
-            <div className="flex flex-row-reverse mt-10 gap-x-5">
-              <Button
-                type="submit"
-                className="bg-gradient-45 text-white rounded-sm"
-              >
-                <span className="flex sm:justify-center items-center gap-2">
-                  Logar
-                </span>
-              </Button>
+              <div className="flex flex-row-reverse mt-10 gap-x-5">
+                <Button
+                  type="submit"
+                  className="bg-gradient-45 text-white rounded-sm"
+                >
+                  <span className="flex sm:justify-center items-center gap-2">
+                    Logar
+                  </span>
+                </Button>
 
-              <Button
-                onClick={createAccount}
-                type="button"
-                className="bg-secondary rounded-sm"
-              >
-                <span className="flex sm:justify-center items-center gap-2">
-                  Criar Conta
-                </span>
-              </Button>
+                <Button
+                  onClick={createAccount}
+                  type="button"
+                  className="bg-secondary rounded-sm"
+                >
+                  <span className="flex sm:justify-center items-center gap-2">
+                    Criar Conta
+                  </span>
+                </Button>
+              </div>
             </div>
-          </div>
-        )}
-      </FormWrapper>
-    </div>
-  );
-}
+          )}
+        </FormWrapper>
+      </div>
+    );
+  }
